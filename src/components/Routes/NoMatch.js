345bb81f8109df86e42f51ebe7497f1ddef842c1
Josh Latour <jlatour88@gmail.com>
2020-06-18 09:42:47 -0500

    Added pathname for debug purposes

--- src/components/Routes/NoMatch.js
+++ src/components/Routes/NoMatch.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const NoMatch = () => {
+  let location = useLocation();
   return (
     <div>
-      <h2>NoMatch Page</h2>
+      <h3>
+        No match for <code>{location.pathname}</code>
+        {/* No match for <code>{window.location.href}</code> */}
+      </h3>
     </div>
   );
 };
