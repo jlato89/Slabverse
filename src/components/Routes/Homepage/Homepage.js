133811e31cd0225fcfdb3558e0f2b91a4a4ce634
jlatour88 <jlatour88@gmail.com>
2020-06-25 12:20:19 -0400

    AddedHeroImage component

--- src/components/Routes/Homepage/Homepage.js
+++ src/components/Routes/Homepage/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HeroImage from './components/HeroImage/HeroImage';
 
 const Homepage = () => {
   return (
     <div>
       <h4>Home Page Content</h4>
+      <HeroImage />
     </div>
   );
 };
